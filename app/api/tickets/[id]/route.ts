d29f49ffb2815319d21b15a5e0609b058aa888b3
sarah_ben <benkanoun.sarah@gmail.com>
2025-08-22 15:26:48 +0100

    change params by context

--- app/api/tickets/[id]/route.ts
+++ app/api/tickets/[id]/route.ts
@@ -4,11 +4,12 @@ import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
  
-export async function PATCH(request: NextRequest, { params } :{ params:{id:string} }){
+export async function PATCH(request: NextRequest, context :{ params:{id:string} }){
   /* const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
   } */
+ const { params } = context;
  const body = await request.json();
  const validation =  patchTicketSchema.safeParse(body);
 
@@ -40,7 +41,8 @@ export async function PATCH(request: NextRequest, { params } :{ params:{id:strin
    });
  return NextResponse.json(updatedTicket)}
 
- export async function DELETE(request: NextRequest, { params } :{ params:{id:string} }){
+ export async function DELETE(request: NextRequest, context :{ params:{id:string} }){
+  const { params } = context;
   const session = await getServerSession(authOptions);
   if (!session) {
     return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
